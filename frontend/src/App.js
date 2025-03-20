9cc79f1eefb63741c808fcd629284fce87c65e86
aj01237 <aj01237@surrey.ac.uk>
2025-03-20 16:07:51 +0000

    Fresh start - added full frontend React app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to MEMS ERP</h1>
+      <p>Start building your ERP system here.</p>
+    </div>
+  );
+}
+
+export default App;
